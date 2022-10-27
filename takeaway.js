17972a002b8e5ec76f1e913ce706307ff77c506d
Greg Stickel <gregory.stickel@live.com>
2022-10-27 09:08:38 -0400

    Initial commit

---
+++ takeaway.js
@@ -0,0 +1,23 @@
+// There’s a game, “takeaway”, that is played by two players, using a number of stones.
+// The first player goes first. She can take 2, 3, or 5 stones from the pool. The second player goes next; she can also remove 2, 3, or 5 stones. The first player then goes, and so on.
+// If a player is unable to move (there are fewer than 2 stones), they lose.
+
+function canWin (n) {
+    if () {
+
+    } else {
+       return; //stop calling 
+    }
+}
+
+
+console.log(canWin(1))// false
+console.log(canWin(2))// true
+console.log(canWin(3))// true
+console.log(canWin(4))// true
+console.log(canWin(5))// true
+console.log(canWin(6))// true
+console.log(canWin(7))// false
+console.log(canWin(8))// false
+console.log(canWin(9))// true
+console.log(canWin(100))// true
